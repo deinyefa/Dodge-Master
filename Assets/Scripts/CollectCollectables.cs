5c99e9ae8b67f9843ae35272c8538f6c7da41637
Deinyefa Eporwei <df.eporwei@gmail.com>
2016-12-06 14:50:29 -0500

    feat: score functionality

---
+++ Assets/Scripts/CollectCollectables.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CollectCollectables : MonoBehaviour {
+
+    int score;
+    public Text scoreText;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Diamond")
+        {
+            score++;
+            scoreText.text = score.ToString();
+            Debug.Log("the score is " + score);
+            Destroy(other.gameObject);
+        }
+    }
+}
